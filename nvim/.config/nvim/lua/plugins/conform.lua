8ec4bf66ee9b367aee1cc169c65dd376017049b8
Lisandro Bertoli <lisandro.bertoli2@promocionesfarma.com>
2024-12-17 09:12:23 +0100

    add yabai config + trouble

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -14,6 +14,12 @@ return { -- Autoformat
     opts = {
         notify_on_error = false,
         format_on_save = function(bufnr)
+            --disable node_modules formatting
+            local buf_name = vim.api.nvim_buf_get_name(bufnr)
+            if buf_name:match '/node_modules' then
+                return
+            end
+
             -- Disable "format_on_save lsp_fallback" for languages that don't
             -- have a well standardized coding style. You can add additional
             -- languages here or re-enable it for the disabled ones.
@@ -25,14 +31,99 @@ return { -- Autoformat
         end,
         formatters_by_ft = {
             lua = { 'stylua' },
-            javascript = { 'prettierd', 'prettier', stop_after_first = true },
-            typescript = { 'prettierd', 'prettier', stop_after_first = true },
-            typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-            javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-            html = { 'prettierd', 'prettier', stop_after_first = true },
-            json = { 'prettierd', 'prettier', stop_after_first = true },
-            markdown = { 'prettierd', 'prettier', stop_after_first = true },
-            graphql = { 'prettierd', 'prettier', stop_after_first = true },
+            typescript = { 'prettierd' },
+            typescriptreact = { 'prettierd' },
+            javascript = { 'prettierd' },
+            javascriptreact = { 'prettierd' },
+            html = { 'prettierd' },
+            css = { 'prettierd' },
+            scss = { 'prettierd' },
+            markdown = { 'prettierd' },
+            yaml = { 'prettierd' },
+            graphql = { 'prettierd' },
+            vue = { 'prettierd' },
+            angular = { 'prettierd' },
+            less = { 'prettierd' },
+            flow = { 'prettierd' },
+            json = { 'prettierd' },
+            sh = { 'beautysh' },
+            bash = { 'beautysh' },
+            zsh = { 'beautysh' },
+            http = { 'kulala-fmt' },
         },
     },
+    config = function(_, opts)
+        local conform = require 'conform'
+        conform.setup(opts)
+
+        -- Customize prettier args
+        require('conform.formatters.prettierd').args = function(_, ctx)
+            local prettier_roots = { '.prettierrc', '.prettierrc.json', 'prettier.config.js' }
+            local args = { '--stdin-filepath', '$FILENAME' }
+            local config_path = vim.fn.stdpath 'config'
+
+            local localPrettierConfig = vim.fs.find(prettier_roots, {
+                upward = true,
+                path = ctx.dirname,
+                type = 'file',
+            })[1]
+            local globalPrettierConfig = vim.fs.find(prettier_roots, {
+                path = type(config_path) == 'string' and config_path or config_path[1],
+                type = 'file',
+            })[1]
+            local disableGlobalPrettierConfig = os.getenv 'DISABLE_GLOBAL_PRETTIER_CONFIG'
+
+            -- Project config takes precedence over global config
+            if localPrettierConfig then
+                vim.list_extend(args, { '--config', localPrettierConfig })
+            elseif globalPrettierConfig and not disableGlobalPrettierConfig then
+                vim.list_extend(args, { '--config', globalPrettierConfig })
+            end
+
+            local hasTailwindPrettierPlugin = vim.fs.find('node_modules/prettier-plugin-tailwindcss', {
+                upward = true,
+                path = ctx.dirname,
+                type = 'directory',
+            })[1]
+
+            if hasTailwindPrettierPlugin then
+                vim.list_extend(args, { '--plugin', 'prettier-plugin-tailwindcss' })
+            end
+
+            return args
+        end
+
+        require('conform.formatters.eslint_d').args = function(_, ctx)
+            local eslint_roots = { '.eslintrc', '.eslintrc.json', '.eslintrc.js' }
+            local args = { '--stdin-filepath', '$FILENAME' }
+            local config_path = vim.fn.stdpath 'config'
+
+            local localEslintConfig = vim.fs.find(eslint_roots, {
+                upward = true,
+                path = ctx.dirname,
+                type = 'file',
+            })[1]
+
+            local globalEslintConfig = vim.fs.find(eslint_roots, {
+                path = type(config_path) == 'string' and config_path or config_path[1],
+                type = 'file',
+            })[1]
+            local disableGlobalEslintConfit = os.getenv 'DISABLE_GLOBAL_PRETTIER_CONFIG'
+
+            -- Project config takes precedence over global config
+            if localEslintConfig then
+                vim.list_extend(args, { '--config', localEslintConfig })
+            elseif globalEslintConfig and not disableGlobalEslintConfit then
+                vim.list_extend(args, { '--config', globalEslintConfig })
+            end
+
+            return args
+        end
+
+        conform.formatters.beautysh = {
+            prepend_args = function()
+                return { '--indent-size', '2', '--force-function-style', 'fnpar' }
+            end,
+        }
+    end,
 }
