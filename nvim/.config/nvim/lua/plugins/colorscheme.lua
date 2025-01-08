3ef7179320fd7ca3517b4a756a866fe566343d8d
Lisandro Bertoli <lisandro.bertoli2@promocionesfarma.com>
2025-01-08 13:36:05 +0100

    remove italics

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,12 @@ return {
     priority = 1000, -- Make sure to load this before all the other start plugins.
     opts = {
         theme = 'dragon',
+        keywordStyle = { bold = true, italic = false },
     },
     init = function()
         vim.cmd.colorscheme 'kanagawa'
 
+        vim.o.background = ''
         vim.cmd.hi 'Comment gui=none'
     end,
 }
