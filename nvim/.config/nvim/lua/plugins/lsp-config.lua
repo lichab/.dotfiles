9e185dcff1c7325e506a47ec466e39284089725a
Lisandro Bertoli <lisandro.bertoli2@promocionesfarma.com>
2025-05-23 09:25:16 +0200

    lsp: php

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -93,6 +93,7 @@ return { -- LSP Configuration & Plugins
             -- But for many setups, the LSP (`tsserver`) will work just fine
             ts_ls = {},
             --
+            intelephense = {},
 
             lua_ls = {
                 -- cmd = {...},
